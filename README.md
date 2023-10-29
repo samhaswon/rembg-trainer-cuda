7c5ed33b1b25df71cfa1c5b89676ae824658e190
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-10-29 14:28:23 +0100

    Refactoring & improvements

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 This code allows you to easily train U2-Net model in [ONNX](https://github.com/onnx/onnx) format to use with [rembg](https://github.com/danielgatis/rembg]) tool
 
-This work is based off [U2Net](https://github.com/xuebinqin/U-2-Net) repo, which is under Apache loicence. The derivative work is licenced under MIT; do whatever you want with it.
+This work is based off [U2Net](https://github.com/xuebinqin/U-2-Net) repo, which is under Apache loicence. The derivative work is licenced under MIT; do as you please with it.
 
-Default parameters are optimized for systems with 32gb of memory, like Apple M1 Pro. Model is saved every 300 iterations. Adjust as necessary.
+Default parameters are fine-tuned for systems with 32gb of memory, like the Apple M1 Pro. Model is saved every 300 iterations. Give it a tweak if you need to.
 
-## How to use
+## Fancy a go?
 
 - Clone this repo
-- Install requirements.txt
+- Install `requirements.txt`
 - Put your images into “images” folder
 - Put your masks into “masks” folder; or see [below](#mask-generation)
-- Adjust parameters of u2net_train.py and then execute it
+- Tinker with the settings of `u2net_train.py` and then give it a whirl.
   - TODO: ability to continue off where you've finished last time
 - Go grab yourself a [nice latte](https://www.youtube.com/shorts/h75W1uhL-iQ) and wait........... and wait.....
-- After you've done waiting, run rembg like this:
+- Once you've had your fill of waiting, here's how you run rembg:
 
 ```bash
 rembg p -w input output -m u2net_custom -x '{"model_path": "/saved_models/u2net/2700.onnx"}'
@@ -26,9 +26,11 @@ rembg p -w input output -m u2net_custom -x '{"model_path": "/saved_models/u2net/
 
 ## Mask extraction
 
-If you already have some images with removed background, then you can create masks off them using supplied alpha.py script. Create directory “clean”, put your pngs there, and launch the script. Be vary: the script is very CPU-heavy. It also requires ImageMagick tool to be installed and present in PATH.
+If you already have a bunch of images with removed background, then you can create masks off them using the provided `alpha.py` script. Create a directory called “clean”, put your pngs there, and launch the script.
 
-Basically, then you will have the following folder structure:
+But fair warning mate: the script is very CPU-heavy. Oh, and you'll need the ImageMagick tool installed and in your PATH.
+
+So, at the end of the day, you will have the following folder structure:
 
 - images — source images, will be needed for training
 - clean — images with removed background, to extract masks
--- u2net_train.py
+++ u2net_train.py
@@ -1,25 +1,35 @@
 import os
 import glob
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
-
+from model import U2NET
 from data_loader import (
     RandomCrop,
     ToTensorLab,
     SalObjDataset,
 )
-from model import U2NET
 
 bce_loss = nn.BCELoss(reduction="mean")
 
 
-def save_model_as_onnx(model, dev, ite_num, input_tensor_size=(1, 3, 320, 320)):
+def get_device():
+    if torch.cuda.is_available():
+        print("CUDA Acceleration enabled")
+        return torch.device("cuda:0")
+    elif torch.backends.mps.is_available():
+        print("Apple M1 acceleration enabled")
+        return torch.device("mps")
+    else:
+        print("No GPU acceleration :/")
+        return torch.device("cpu")
+
+
+def save_model_as_onnx(model, device, ite_num, input_tensor_size=(1, 3, 320, 320)):
     x = torch.randn(*input_tensor_size, requires_grad=True)
-    x = x.to(dev)
+    x = x.to(device)
 
     onnx_file_name = "saved_models/{}.onnx".format(ite_num)
     torch.onnx.export(
@@ -36,51 +46,87 @@ def save_model_as_onnx(model, dev, ite_num, input_tensor_size=(1, 3, 320, 320)):
     print("Model saved to:", onnx_file_name)
 
 
+def load_dataset(img_dir, lbl_dir, ext=".png"):
+    img_list = glob.glob(os.path.join(img_dir, "*" + ext))
+    lbl_list = [os.path.join(lbl_dir, os.path.basename(img)) for img in img_list]
+
+    return img_list, lbl_list
+
+
 def muti_bce_loss_fusion(d_list, labels_v):
     losses = [bce_loss(d, labels_v) for d in d_list]
     total_loss = sum(losses)
     return losses[0], total_loss
 
 
+def train_model(net, optimizer, dataloader, device, epoch_num, save_frq):
+    iteration_count = 0
+    cumulative_loss = 0.0
+    epoch_loss = 0.0
+
+    for epoch in range(epoch_num):
+        net.train()
+
+        for i, data in enumerate(dataloader):
+            iteration_count += 1
+
+            inputs = data["image"].to(device).float()
+            labels = data["label"].to(device).float()
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+
+            first_output, combined_loss = muti_bce_loss_fusion(outputs, labels)
+            combined_loss.backward()
+            optimizer.step()
+
+            epoch_loss += combined_loss.item()
+            cumulative_loss += combined_loss.item()
+
+            print(
+                f"[Epoch: {epoch + 1}/{epoch_num}, Iteration: {iteration_count}/{len(dataloader)/dataloader.batch_size*(epoch+1)}] "
+                f"Epoch Avg Loss: {epoch_loss / iteration_count}, "
+                f"Cumulative Avg: {cumulative_loss / iteration_count}, "
+            )
+
+            # Saves model every save_frq iterations
+            if iteration_count % save_frq == 0:
+                save_model_as_onnx(
+                    net, device, iteration_count
+                )  # in ONNX format! ^_^ UwU
+                print("Model saved")
+
+        epoch_loss = 0.0
+
+    save_model_as_onnx(net, device, iteration_count)
+    print("Model saved for the last time")
+
+    return net
+
+
 def main():
-    # Choosing backend
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-        print("CUDA Acceleration enabled")
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
-        print("Apple M1 acceleration enabled")
-    else:
-        device = torch.device("cpu")
-        print("No GPU acceleration :/")
+    device = get_device()
 
     # Directories and model specifications
     tra_image_dir = "images"
     tra_label_dir = "masks"
     image_ext = ".png"
-    label_ext = ".png"
-    epoch_num = 50
+    epoch_num = 200
     save_frq = 300
     batch = (
         20  # Affects VRAM usage! 20 uses ~20+ gb of VRAM. Reduce to suit your hardware.
     )
 
-    tra_img_name_list = glob.glob(os.path.join(tra_image_dir, "*" + image_ext))
-
-    tra_lbl_name_list = []
-    for img_path in tra_img_name_list:
-        img_name = img_path.split(os.sep)[-1]
-
-        aaa = img_name.split(".")
-        bbb = aaa[0:-1]
-        imidx = bbb[0]
-        for i in range(1, len(bbb)):
-            imidx = imidx + "." + bbb[i]
-
-        tra_lbl_name_list.append(os.path.join(tra_label_dir, imidx + label_ext))
+    tra_img_name_list, tra_lbl_name_list = load_dataset(
+        tra_image_dir, tra_label_dir, image_ext
+    )
 
     print("Images: ", len(tra_img_name_list))
     print("Masks: ", len(tra_lbl_name_list))
+
+    if len(tra_img_name_list) != len(tra_lbl_name_list):
+        print("Different amounts of images and masks, can't proceed mate")
+        return
 
     salobj_dataset = SalObjDataset(
         img_name_list=tra_img_name_list,
@@ -105,47 +151,7 @@ def main():
     # Training loop
     print("Launching...")
     print("---")
-
-    ite_num = 0
-    running_loss = 0.0
-    running_tar_loss = 0.0
-
-    for epoch in range(epoch_num):
-        net.train()
-
-        for i, data in enumerate(salobj_dataloader):
-            ite_num += 1
-
-            inputs, labels = data["image"].type(torch.FloatTensor).to(device), data[
-                "label"
-            ].type(torch.FloatTensor).to(device)
-
-            if torch.cuda.is_available():
-                inputs, labels = inputs.cuda(), labels.cuda()
-
-            optimizer.zero_grad()
-            outputs = net(inputs)
-
-            loss2, loss = muti_bce_loss_fusion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-            running_tar_loss += loss2.item()
-
-            print(
-                f"[Epoch: {epoch + 1}/{epoch_num}, Iteration: {ite_num}] Train Loss: {running_loss/ite_num}, Target Loss: {running_tar_loss/ite_num}"
-            )
-
-            # Saves model every save_frq iterations
-            if ite_num % save_frq == 0:
-                save_model_as_onnx(net, device, ite_num)  # in ONNX format! ^_^ UwU
-                print("Model saved")
-                running_loss = 0.0
-                running_tar_loss = 0.0
-
-    save_model_as_onnx(net, device, ite_num)
-    print("Model saved for the last time")
+    train_model(net, optimizer, salobj_dataloader, device, epoch_num, save_frq)
 
 
 if __name__ == "__main__":
