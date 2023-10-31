63e664587c0f9c797bfea7db376a5949134801a5
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-10-31 11:42:03 +0100

    Comments are added :3

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,6 @@
+"""
+Helper code for loading and altering images in dataset
+"""
 import gc
 import random
 
@@ -9,10 +12,29 @@ from torch.utils.data import Dataset
 
 
 class RandomCrop:
-    THRESHOLDS = [0.5, 0.8, 0.9, 0.95, 0.98, 0.99]
-    start_threshold_index = 0  # variable to keep track of the current threshold index
+    """
+    A class for performing random cropping of given image.
+    Treats white pixels as empty space, and strives to return as few of them as possible.
+    """
+
+    THRESHOLDS = [
+        0.5,
+        0.8,
+        0.9,
+        0.95,
+        0.98,
+        0.99,
+    ]  # allowed percentages of white pixels
+    start_threshold_index = 0  # chosen percentage that we aim for
 
     def __init__(self, output_size, index=0):
+        """
+        Initialize the RandomCrop transformer.
+
+        Parameters:
+        - output_size (int or tuple): The desired size of the cropped image.
+        - index (int): The starting threshold index.
+        """
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
             self.output_size = (output_size, output_size)
@@ -22,44 +44,63 @@ class RandomCrop:
 
     @staticmethod
     def _calculate_white_percentage(img):
+        """Calculate the percentage of white pixels in the image."""
         white_pixels = np.sum(img == 255)
         total_pixels = img.size
         return white_pixels / total_pixels
 
     def __call__(self, sample):
+        """
+        Apply the random crop to the input image + mask.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the random crop of image and mask.
+        """
         image, label = sample["image"], sample["label"]
 
         w, h = image.size
-        grid_size = self.output_size[
-            0
-        ]  # Assuming you want the grid to be of the same size as the crop
+        grid_size = self.output_size[0]
 
+        # splitting image into grid for faster search
         cells = [(i, j) for i in range(0, w, grid_size) for j in range(0, h, grid_size)]
         random.shuffle(cells)
 
-        threshold_sequence = (RandomCrop.THRESHOLDS[RandomCrop.start_threshold_index:])
-        try:
-            for threshold in threshold_sequence:
-                for i, j in cells:
-                    if i + self.output_size[0] <= w and j + self.output_size[1] <= h:
-                        cropped_image = tf.crop(image, i, j, *self.output_size)
-                        cropped_label = tf.crop(label, i, j, *self.output_size)
+        # looking for as non-empty cell as possible
+        # lowering threshold of whiteness if none are found
+        threshold_sequence = RandomCrop.THRESHOLDS[RandomCrop.start_threshold_index :]
+        for threshold in threshold_sequence:
+            for i, j in cells:
+                if i + self.output_size[0] <= w and j + self.output_size[1] <= h:
+                    cropped_image = tf.crop(image, i, j, *self.output_size)
+                    cropped_label = tf.crop(label, i, j, *self.output_size)
 
-                        if (
-                            self._calculate_white_percentage(np.array(cropped_image))
-                            <= threshold
-                        ):
-                            return {"image": cropped_image, "label": cropped_label}
+                    if (
+                        self._calculate_white_percentage(np.array(cropped_image))
+                        <= threshold
+                    ):
+                        return {"image": cropped_image, "label": cropped_label}
 
-            raise ValueError("Fully white image is given :(")
-
-        except ValueError as e:
-            print(f"Error: {str(e)}")
-            raise
+        raise ValueError("Fully white image is given :(")
 
 
 class HorizontalFlip:
+    """
+    A class to perform horizontal flipping of images.
+    """
+
     def __call__(self, sample):
+        """
+        Flip the image and mask horizontally.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the horizontally flipped image and mask.
+        """
         image, label = sample["image"], sample["label"]
 
         # Apply horizontal flip
@@ -70,7 +111,20 @@ class HorizontalFlip:
 
 
 class VerticalFlip:
+    """
+    A class to perform vertical flipping of images.
+    """
+
     def __call__(self, sample):
+        """
+        Flip the image and mask vertically.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the vertically flipped image and mask.
+        """
         image, label = sample["image"], sample["label"]
 
         # Apply vertical flip
@@ -81,10 +135,29 @@ class VerticalFlip:
 
 
 class Rotation:
+    """
+    A class to rotate images by a given angle.
+    """
+
     def __init__(self, degrees):
+        """
+        Initialize the Rotation transformer.
+
+        Parameters:
+        - degrees (float): The angle by which the image should be rotated.
+        """
         self.degrees = degrees
 
     def __call__(self, sample):
+        """
+        Rotate the image and mask by a specified angle.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the rotated image and mask.
+        """
         image, label = sample["image"], sample["label"]
 
         # Apply rotation
@@ -95,10 +168,29 @@ class Rotation:
 
 
 class Resize:
+    """
+    A class to resize images to a specified size.
+    """
+
     def __init__(self, size=1024):
+        """
+        Initialize the Resize transformer.
+
+        Parameters:
+        - size (int): The desired size of the image after resizing.
+        """
         self.size = size
 
     def __call__(self, sample):
+        """
+        Resize the image and mask to the specified size.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the resized image and mask.
+        """
         image, label = sample["image"], sample["label"]
 
         # Resize both the image and label
@@ -109,26 +201,66 @@ class Resize:
 
 
 class ToTensorLab:
+    """
+    A class to convert images from PIL format to PyTorch tensors.
+    """
+
     def __call__(self, sample):
+        """
+        Convert the image and label from PIL format to PyTorch tensors.
+
+        Parameters:
+        - sample (dict): Dictionary containing an image and its mask.
+
+        Returns:
+        - Dictionary containing the image and mask as tensors.
+        """
         image, label = sample["image"], sample["label"]
 
         # Convert to tensor
         image = tf.to_tensor(image)
         label = tf.to_tensor(label)
+        # This is where you apply half precision if needed!
 
         return {"image": image, "label": label}
 
 
 class SalObjDataset(Dataset):
+    """
+    Custom dataset class for salient object detection. This class helps in
+    loading images, their corresponding masks, and applying the desired
+    transformations before feeding them to the network.
+    """
+
     def __init__(self, img_name_list, lbl_name_list, transform=None):
+        """
+        Initialize the SalObjDataset.
+
+        Parameters:
+        - img_name_list (list): List of paths to the images.
+        - lbl_name_list (list): List of paths to the corresponding masks.
+        - transform (callable, optional): Optional transform to be applied to both image & mask.
+        """
         self.img_name_list = img_name_list
         self.lbl_name_list = lbl_name_list
         self.transform = transform
 
     def __len__(self):
+        """Return the total number of images in the dataset."""
         return len(self.img_name_list)
 
     def __getitem__(self, idx):
+        """
+        Fetch an image and its corresponding label, apply any transformations if needed,
+        and return them as a dictionary.
+
+        Parameters:
+        - idx (int): Index of the desired sample.
+
+        Returns:
+        - Dictionary containing an image and its label.
+        """
+        # Read the images
         image_array = iio.imread(self.img_name_list[idx])
         label_array = iio.imread(self.lbl_name_list[idx])
 
@@ -136,23 +268,14 @@ class SalObjDataset(Dataset):
         image = Image.fromarray(image_array)
         label = Image.fromarray(label_array)
 
+        # Clean up memory
         del image_array, label_array
         gc.collect()
 
-        # If your images aren't already RGB or grayscale, you can convert them:
-        # image = image.convert("RGB")
-        # label = label.convert("L")
-
         sample = {"image": image, "label": label}
 
+        # Apply the transformations
         if self.transform:
             sample = self.transform(sample)
-
-        # save_dir = "transformed_images"
-        # os.makedirs(save_dir, exist_ok=True)
-
-        # Save the transformed image and label
-        # save_image(sample["image"], os.path.join(save_dir, f"transformed_image_{idx}.png"))
-        # save_image(sample["label"], os.path.join(save_dir, f"transformed_label_{idx}.png"))
 
         return sample
